a369adadb8e8c856f728f6f0dedc0782f828afea
Your Name <you@example.com>
2025-05-16 00:55:13 +0900

    Add template for ros2 deployment

---
+++ ros2_ws/src/joint_planner/src/Planner/Planner.h
@@ -0,0 +1,27 @@
+#ifndef PLANNER_H
+#define PLANNER_H
+
+#include <iostream>
+#include <vector>
+
+namespace planner 
+{
+
+    class Planner 
+    {
+        public:
+            Planner();
+
+            template <typename T>
+            T compute(float dt);
+            void reset();
+
+        private:
+            float _current_time;
+
+            void _initialize();
+    };
+
+} // namespace Planner
+
+#endif // PLANNER_H
---
+++ ros2_ws/src/joint_planner/src/joint_planner_node.cpp
@@ -0,0 +1,31 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "ActuatorInterface/ROSActuatorInterface.h"
+#include "Planner/Planner.h"
+#include "StateMachine/StateMachine.h"
+
+
+int main(int argc, char *argv[])
+{
+    /* Initialize ros2 node */
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("joint_planner_node");
+
+    /* Create the actuator interface */
+    auto actuator_interface = std::make_shared<ROSActuatorInterface>(node);
+
+    /* Create the planner */
+    auto planner = std::make_shared<planner::Planner>();
+
+    /* Create the state machine */
+    auto state_machine = std::make_shared<planner_sm::PlannerStateMachine>(node, actuator_interface, planner);
+
+    while(rclcpp::ok())
+    {
+        state_machine->event_loop(); // Process the state machine events
+        rclcpp::spin_some(node); // Process any incoming messages
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
